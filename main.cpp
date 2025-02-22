750ccaea4be6a45884e614e69995f0f7d13be7ff
nghthanhlong <nghiemlong2006@gmail.com>
2025-02-22 08:31:14 +0700

    Dictionary added & get random word

--- main.cpp
+++ main.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
-
+#include <vector>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
+
+const char *name = "dictionary.txt";
+
+string getWord(const char *filename)
+{
+    vector<string> wordList;
+    ifstream file(filename);
+
+    if(file.is_open())
+    {
+        string word;
+        while(file>>word)
+        {
+            wordList.push_back(word);
+        }
+    }
+
+    file.close();
+    if(wordList.size()>0) {
+        int randomIdx=rand()%wordList.size();
+        return wordList[randomIdx];
+    }
+    else return "";
+}
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    srand(time(0));
+    cout<<getWord(name);
     return 0;
 }
