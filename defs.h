9759ccab4f2712124b52bb0cd1c7005824dc7194
nghthanhlong <nghiemlong2006@gmail.com>
2025-03-14 16:29:54 +0700

    Size of screen update

--- defs.h
+++ defs.h
@@ -1,12 +1,13 @@
 #ifndef DEFS_H_INCLUDED
 #define DEFS_H_INCLUDED
 
-const char *name = "dictionary.txt";
-const char *backgroundName = "background.jpg"
-const int WINDOW_WIDTH = 800;
+//const char *dictionarySet = "dictionary.txt";
+//const char *backgroundName = "background.jpg";
+const int WINDOW_WIDTH = 850;
 const int WINDOW_HEIGHT = 600;
 const int CELL_SIZE = 50;
 const int GRID_COLS = 5;
 const int GRID_ROWS = 6;
+
 
 #endif // DEFS_H_INCLUDED
