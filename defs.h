9b692f203414fd7a5c35da333b0fa33ce289c53d
nghthanhlong <nghiemlong2006@gmail.com>
2025-04-07 21:13:48 +0700

    Change dictionary, change gameplay
    6x8 board

--- defs.h
+++ defs.h
@@ -1,12 +1,10 @@
 #ifndef DEFS_H_INCLUDED
 #define DEFS_H_INCLUDED
 
-//const char *backgroundName = "background.jpg";
 const int WINDOW_WIDTH = 850;
 const int WINDOW_HEIGHT = 600;
 const int CELL_SIZE = 50;
-const int GRID_COLS = 5;
-const int GRID_ROWS = 6;
-
+const int GRID_COLS = 6;
+const int GRID_ROWS = 8;
 
 #endif // DEFS_H_INCLUDED
