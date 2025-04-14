a5a8b08ca5fc637f872004a28fa617875761ffad
nghthanhlong <nghiemlong2006@gmail.com>
2025-04-14 21:46:24 +0700

    Add distance constant, move const char variable

--- defs.h
+++ defs.h
@@ -6,5 +6,12 @@ const int WINDOW_HEIGHT = 600;
 const int CELL_SIZE = 50;
 const int GRID_COLS = 6;
 const int GRID_ROWS = 8;
+const int DISTANCE=20;
+const int DISTANCE_WINDOW_AND_LINE=40;
+
+inline const char *WINDOW_TITLE = "Hardle";
+inline const char *fontUsed = "assets/PatrickHand-Regular.ttf";
+inline const char *backgroundUsed="assets/background.jpg";
+
 
 #endif // DEFS_H_INCLUDED
